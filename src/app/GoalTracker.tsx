d3c547cd70b77ef400894e618ffa00833f04abce
AadityaMohit <150881374+AadityaMohit@users.noreply.github.com>
2025-05-02 13:44:22 +0530

    Initial commit

--- src/app/GoalTracker.tsx
+++ src/app/GoalTracker.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
